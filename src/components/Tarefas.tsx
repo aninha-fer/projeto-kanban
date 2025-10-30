afcbcc8d43a7b9008fd09535ec84a19ff11d54b4
Ana Carolina <anafer.digital@gmail.com>
2025-10-29 21:38:31 -0300

    final da aula

---
+++ src/components/Tarefas.tsx
@@ -0,0 +1,3 @@
+export default function Tarefas() {
+    return "tarefas";
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import Tarefas from "../components/Tarefas";
+
+export default function Home() {
+    return (
+        <div>
+            'home'
+            <Tarefas/>
+        </div>
+    );
+}
